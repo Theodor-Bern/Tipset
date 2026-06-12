15090ab75e76503081e1d5bba22c58b4be6ffa49
Theodor-Bern <theodor.bernelind.0642@student.uu.se>
2026-06-12 14:50:03 +0200

    Added a notes endpoint

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+notes = []
+
+class NoteCreate(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/health")
-async def health():
+def health():
     return {"message": "healthy"}
 
+@app.post("/notes")
+def create_note(note: NoteCreate):
+    notes.append(note)
+    return note
+
+@app.get("/notes")
+def list_notes():
+    return notes
