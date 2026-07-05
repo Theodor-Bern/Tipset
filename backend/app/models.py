bc5e7f400b05377c8f1b4b691d92343c75270b81
Theodor-Bern <theodor.bernelind.0642@student.uu.se>
2026-07-05 13:39:41 +0200

    Added a foreign key to connect users and notes

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import  Mapped, mapped_column
-from sqlalchemy import String, Integer
+from sqlalchemy.orm import  Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer, ForeignKey
 from app.database import Base
 
 class Note(Base):
@@ -8,6 +8,8 @@ class Note(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     title: Mapped[str] = mapped_column(String)
     content: Mapped[str] = mapped_column(String)
+    user_id: Mapped[int | None] = mapped_column(ForeignKey("users.id"))
+    owner: Mapped["User | None"] = relationship(back_populates="notes")
     
 class User(Base):
     __tablename__ = "users"
@@ -15,3 +17,4 @@ class User(Base):
     email: Mapped[str] = mapped_column(String, unique=True, index=True)
     username: Mapped[str] = mapped_column(String, unique=True, index=True)
     password_hash: Mapped[str] 
+    notes: Mapped[list["Note"]] = relationship(back_populates="owner")
